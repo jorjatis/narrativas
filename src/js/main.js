c6005bd79671cee2d2f7cf58cc8700561fcb766e
Jorge Garcia <jgarcia@abc.es>
2025-12-17 17:30:38 +0100

    narrativa done

--- src/js/main.js
+++ src/js/main.js
@@ -10,4 +10,18 @@ document.addEventListener('DOMContentLoaded', () => {
     title.insertAdjacentElement('afterend', subtitle);
   }
 
+  // --------------------------------
+  // Cambiar titulo para la animación
+  // --------------------------------
+  if (title) {
+    title.innerHTML = `
+      <div class="text-fixed">La batalla contra</div>
+      <div class="text-anim">
+        <div class="word word--1">los demonios</div>
+        <div class="word word--2">las dudas</div>
+        <div class="word word--3">las angustias</div>
+        <div class="word word--4">el miedo</div>
+      </div>
+    `;
+  }
 });
