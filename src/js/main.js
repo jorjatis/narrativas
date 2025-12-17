ae69e399a215a195de052fabb36355d3cf655f97
Jorge Garcia <jgarcia@abc.es>
2025-12-17 15:51:34 +0100

    update main

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
-console.log('hasdasdola');
+document.addEventListener('DOMContentLoaded', () => {
+  // --------------------------------
+  // Mover elementos
+  // --------------------------------
+  // Mover subtitle debajo del title
+  const title = document.querySelector('.voc-title');
+  const subtitle = document.querySelector('.voc-subtitle');
+
+  if (title && subtitle) {
+    title.insertAdjacentElement('afterend', subtitle);
+  }
+
+});
