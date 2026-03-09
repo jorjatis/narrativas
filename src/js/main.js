24e0a37648a857f495a7651440a83ac5a8d6ff2e
Jorge Garcia <jgarcia@abc.es>
2026-03-09 22:47:22 +0100

    next

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
-﻿import { initThemeToggle } from './modules/theme-toggle';
+import { initThemeToggle } from './modules/theme-toggle';
 import { initCardPreviewHover } from './modules/card-preview';
+import { initTagSearch } from './modules/tag-search';
 
 function initAll() {
   initThemeToggle();
   initCardPreviewHover();
+  initTagSearch();
 }
 
 document.addEventListener('DOMContentLoaded', () => {
