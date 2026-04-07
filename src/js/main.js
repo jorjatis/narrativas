a321d247bf0b6214c83a6c57c1994bc69c387869
Jorge Garcia <jgarcia@abc.es>
2026-04-07 15:48:05 +0200

    init

--- src/js/main.js
+++ src/js/main.js
@@ -3,23 +3,9 @@ import moveEls from './helpers/moveEls';
 import fadeOnScroll from './helpers/fadeOnScroll';
 
 // NOTE: Components
-import dropdown from './modules/dropdown';
-import accordion from './modules/accordion';
-
-import dataRoutes from './modules/ssRoutes-data';
-import ssRoutes from './modules/ssRoutes';
-
-import dataMap from './modules/ssMap-data';
-import ssMap from './modules/ssMap';
 
 export function initAll() {
-  moveEls('.v-a--d-s-1 .v-a-s-t', '.v-d--abc', "prepend");
-  moveEls('.v-a--d-s-1 .v-n-pre-a-ath', '.v-d--abc .v-a-s-t', "after");
-  fadeOnScroll('.scr-ind');
-  dropdown();
-  accordion();
-  ssRoutes(dataRoutes);
-  ssMap(dataMap);
+
 }
 
 function start() {
