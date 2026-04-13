a99fea439e8ce9e1612f32d74797aa366c6e6ae9
dblanco <dblanco@abc.es>
2026-04-13 08:53:00 +0200

    Ultimos arreglos

--- src/views/helpers/inc.js
+++ src/views/helpers/inc.js
@@ -1,6 +1,6 @@
 // Incrementa un valor en 1 (util para los @index) 
 // Uso:
-// <img src="assets/images/doctor-{{inc @index}}.png" alt="">  → Empezar en el 1, en vez de en el 0
+// <img src="https://s1.abcstatics.com/comun/especiales/bandas-latinas/images/doctor-{{inc @index}}.png" alt="">  → Empezar en el 1, en vez de en el 0
 
 module.exports = function(value) {
   return parseInt(value, 10) + 1;
