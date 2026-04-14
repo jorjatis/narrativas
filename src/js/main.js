7b48d030074b1569c22d7e21993ce59ce7ecdbdd
Jorge Garcia <jgarcia@abc.es>
2026-04-14 18:33:18 +0200

    block 10 and 11 done

--- src/js/main.js
+++ src/js/main.js
@@ -14,47 +14,47 @@ export function initAll() {
   removeEls('.v-a--d-s-1 > .v-a-inf-c');
   fadeOnScroll('.scr-ind');
 
-  parallaxPreHeader();
-  animateFidelAtril();
-  animateMapamundiPlato();
+  // parallaxPreHeader();
+  // animateFidelAtril();
+  // animateMapamundiPlato();
   scrolly();
 
-  // Efecto blur in en textos
-  observeInView({
-    target: '.v-n-preh-txt p.blur-y-in',
-    threshold: 0.4,
-    once: true,
-    onEnter: (entry) => {
-      entry.target.classList.add('is-visible');
-    }
-  });
+  // // Efecto blur in en textos
+  // observeInView({
+  //   target: '.v-n-preh-txt p.blur-y-in',
+  //   threshold: 0.4,
+  //   once: true,
+  //   onEnter: (entry) => {
+  //     entry.target.classList.add('is-visible');
+  //   }
+  // });
 
-  // Efecto blur in en info-container
-  observeInView({
-    target: '.v-a--d-s-1 .v-a-img-c .v-a-inf-c.blur-y-in',
-    threshold: 0.5,
-    once: true,
-    onEnter: (entry) => {
-      entry.target.classList.add('is-visible');
-    }
-  });
+  // // Efecto blur in en info-container
+  // observeInView({
+  //   target: '.v-a--d-s-1 .v-a-img-c .v-a-inf-c.blur-y-in',
+  //   threshold: 0.5,
+  //   once: true,
+  //   onEnter: (entry) => {
+  //     entry.target.classList.add('is-visible');
+  //   }
+  // });
 
-  // Lottie grafiti
-  const anim = loadLottie({
-    container: '#lottie-grafiti',
-    path: 'https://s1.abcstatics.com/comun/narrativas/redaccion/2026/04/19/cubacontracuba/lotties/1-cabecera-gota-grafiti.json',
-    loop: false,
-    autoplay: false
-  });
+  // // Lottie grafiti
+  // const anim = loadLottie({
+  //   container: '#lottie-grafiti',
+  //   path: 'https://s1.abcstatics.com/comun/narrativas/redaccion/2026/04/19/cubacontracuba/lotties/1-cabecera-gota-grafiti.json',
+  //   loop: false,
+  //   autoplay: false
+  // });
 
-  observeInView({
-    target: '#lottie-grafiti',
-    threshold: 0.5,
-    once: true,
-    onEnter: () => {
-      anim?.play();
-    }
-  });
+  // observeInView({
+  //   target: '#lottie-grafiti',
+  //   threshold: 0.5,
+  //   once: true,
+  //   onEnter: () => {
+  //     anim?.play();
+  //   }
+  // });
 }
 
 function start() {
--- src/js/modules/animateMapamundiPlato.js
+++ src/js/modules/animateMapamundiPlato.js
@@ -7,30 +7,15 @@ export default function animateMapamundiPlato() {
   const tl = gsap.timeline({
     scrollTrigger: {
       trigger: ".v-n-mth--11 figure",
-      start: "top 80%",
-      end: "30% 70%",
+      start: "25% center",
+      end: "bottom center",
       scrub: true
     }
   });
 
-  const tl2 = gsap.timeline({
-    scrollTrigger: {
-      trigger: ".v-n-mth--11 figure",
-      start: "40% 80%",
-      end: "80% 70%",
-      scrub: true
-    }
-  });
-
-  tl.to(".v-n-mth--11 figure", {
-    rotate: 0,
+  tl.to(".v-n-mth--11 .v-n-img--1", {
+    rotate: 180,
     ease: "none",
     duration: 1 
-  });
-
-  tl2.to(".v-n-img--2", {
-    rotate: -0,
-    ease: "none",
-    duration: 1
-  });
+  })
 }
