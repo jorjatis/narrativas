53f6e852b693002f8cb119ccf220e4a9230f0126
Jorge Garcia <jgarcia@abc.es>
2026-04-23 15:39:16 +0200

    fix

--- src/js/modules/imagesUnblur.js
+++ src/js/modules/imagesUnblur.js
@@ -4,12 +4,26 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 export default function imagesUnblur() {
   gsap.registerPlugin(ScrollTrigger);
 
+  ScrollTrigger.config({
+    autoRefreshEvents: "visibilitychange,DOMContentLoaded,load"
+  });
+
   const allMuralImages = document.querySelectorAll('.v-n-preh__img');
   const muralContainer = document.querySelector('.v-n-preh');
   const titleBlock = document.querySelector('.v-a--d-s-1 .v-a-inf-c');
   const section4 = document.querySelector('.v-n-preh__sec--4');
 
   if (!muralContainer || allMuralImages.length === 0 || !titleBlock || !section4) return;
+
+  let scrollY = 0;
+
+  ScrollTrigger.addEventListener("refreshInit", () => {
+    scrollY = window.scrollY;
+  });
+
+  ScrollTrigger.addEventListener("refresh", () => {
+    window.scrollTo(0, scrollY);
+  });
 
   const createAnimation = (endTrigger, end) => {
     if (!endTrigger) return;
@@ -32,4 +46,6 @@ export default function imagesUnblur() {
     "(min-width: 700px)": () => createAnimation(titleBlock, 'top bottom'),
     "(max-width: 699px)": () => createAnimation(section4, 'center center')
   });
+
+  ScrollTrigger.matchMediaRefresh();
 }
