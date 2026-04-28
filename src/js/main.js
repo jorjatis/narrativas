188cfe8160458c9ec4432d72ccb424cd63114b49
Jorge Garcia <jgarcia@abc.es>
2026-04-28 11:17:42 +0200

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,7 @@
 import { initThemeToggle } from './modules/theme-toggle';
-import { initCardPreviewHover } from './modules/card-preview';
 
 function initAll() {
   initThemeToggle();
-  initCardPreviewHover();
 }
 
 document.addEventListener('DOMContentLoaded', () => {
