82c9dd27d350ef515b633651ab088ebd131dafb9
Jorge Garcia <jgarcia@abc.es>
2026-06-01 14:34:30 +0200

    init

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,19 @@
 // NOTE: Helpers
 import fadeOnScroll from './helpers/fadeOnScroll';
+import moveEls from './helpers/moveEls';
+import removeEls from './helpers/removeEls';
 
 // NOTE: Utils
-import overflowable from './utils/overflowable';
-import descriptionOverflow from './utils/descriptionOverflow';
 
 // NOTE: Modules
-import papaViewer from './modules/papa-viewer';
 
 export function initAll() {
   fadeOnScroll('.scr-ind');
-
-  papaViewer();
-
-  overflowable();
-
-  descriptionOverflow();
+  moveEls('.v-a--d-s-1 .v-a-inf-c .v-a-s-t', '.v-d--abc .v-ath__w--2', 'before');
+  removeEls('.v-a--d-s-1 .v-a-inf-c > .v-i')
 }
 
 // NOTE: Para prod el evento load de window, se borra, dejamos solo:
 // initAll();
 
-window.addEventListener(
-  'load',
-  initAll
-);
+window.addEventListener('load',  initAll);
