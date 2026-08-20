586f156358a93c17d9903f347adc53f7a6d555ba
Jorge Garcia <jgarcia@abc.es>
2026-08-20 15:29:44 +0200

    init

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,9 @@
 // NOTE: Helpers
-import fadeOnScroll from './helpers/fadeOnScroll';
-import removeElements from './helpers/removeEls';
 
 // NOTE: Modules
-import initAudioPlayer from './modules/audio-player';
-import initVideoSubtitles from './modules/video-subtitles';
-import initPreArticleHeader from './modules/pre-article-header';
-import initHasPaywall from './modules/hasPaywall';
-import initRouteMediasMap from './modules/route-medias-map';
-import initRouteMediasVideoModal from './modules/route-medias-video-modal';
 
 export function initAll() {
-  document.body.classList.add('is-loaded');
 
-  removeElements('.v-a--d-s-1 > .v-a-inf-c');
-  fadeOnScroll(".v-a--d-s-1 .v-a-img-c > .scr-ind");
-  initAudioPlayer();
-  initVideoSubtitles();
-  initPreArticleHeader();
-  initHasPaywall();
-  initRouteMediasMap();
-  initRouteMediasVideoModal();
 }
 
 // NOTE: Para prod el evento load de window se borra, dejamos solo:
