20b8aadc2f90c0e71bf150484c1c2f9748d5b629
Jorge Garcia <jgarcia@abc.es>
2026-09-15 13:40:58 +0200

    init and ladillo and sumario and image xl figcaption

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,12 @@
 // NOTE: Helpers
-import syncNarrativeData from "./helpers/syncNarrativeData";
 import fadeOnScroll from "./helpers/fadeOnScroll";
 
 // NOTE: Modules
-import preHeaderScene from "./modules/preHeaderScene";
 
 export function initAll() {
-  syncNarrativeData();
-
   document.body.classList.add("is-loaded");
 
   fadeOnScroll(".v-n-preh .scr-ind");
-  
-  preHeaderScene();
 }
 
 // NOTE: Para prod el evento load de window se borra, dejamos solo:
